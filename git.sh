e1a8d29e94754ea9840500e28ea26322096ac124
Blinov Evgeniy <eblinov@alvioneurope.ru>
2015-04-10 17:55:45 +0300

    master Work stage 2015-04-10 17:55:45  M git.sh

--- git.sh
+++ git.sh
@@ -3,7 +3,7 @@
 
 function git_auto_commit {
     git commit -m "$(cat <<END_HEREDOC
-$(git rev-parse --abbrev-ref HEAD)
+$(git rev-parse --abbrev-ref HEAD|awk -F '_' '{print "[#"$3"]"}')  $(git rev-parse --abbrev-ref HEAD)
 Work stage $(date '+%F %T')
 $(git status -s)
 END_HEREDOC
