24f866b22884242fa10eaeaf9081a0afb15cfb82
Blinov Evgeniy <evgeniy_blinov@mail.ru>
2016-07-10 11:04:30 +0300

    [#]  master Work stage 2016-07-10 11:04:30 A  composer.sh  M irc-linux.sh

--- irc-linux.sh
+++ irc-linux.sh
@@ -3,5 +3,10 @@
 irc-linux() {
     local day_back="$1"
     [ -z "$day_back" ] && day_back=0
-    wget -qO- $(LANG=C TZ=UTC date --date="-${day_back} day" +http://irc.linsovet.org.ua/logs/linux_raw/linux.log.%d%b%Y.txt)
+    #wget -qO- $(LANG=C TZ=UTC date --date="-${day_back} day" +http://irc.linsovet.org.ua/logs/linux_raw/linux.log.%d%b%Y.txt)
+    wget -qO- $(LANG=C TZ=UTC date --date="-${day_back} day" +http://irc.linsovet.org.ua/logs/linux_raw/linux.log.%d%b%Y.txt)|
+    grep -v '#linux: mode change'|
+    grep -vP '\([^\)]+\) joined #linux'|
+    grep -vP '\([^\)]+\) left irc:'|
+    grep -vP 'kicked from #linux by bt:'
 }
