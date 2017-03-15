e9a9ee8856e5dbf29a1b0c3f87a8631b88b082be
Blinov Evgeniy <evgeniy_blinov@mail.ru>
2017-03-15 18:51:06 +0300

    [#]  master : add changes to commit
    Work stage 2017-03-15 18:51:06
     M git.sh

--- git.sh
+++ git.sh
@@ -25,6 +25,7 @@ function git_auto_commit {
         -${ALL}m "$(cat <<END_HEREDOC
 $(git rev-parse --abbrev-ref HEAD|awk -F '_' '{print "[#"$3"]"}')  $(git rev-parse --abbrev-ref HEAD) $MESSAGE
 
+
 Work stage $(date '+%F %T')
 $(git status -s)
 END_HEREDOC
