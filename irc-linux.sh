1fc3bbaf3f59f808bf082f323b9df4dc440b232d
Evgeniy Blinov <evgeniy.blinov@arcadia.spb.ru>
2018-03-26 12:51:55 +0300

    master
    Work stage 2018-03-26 12:51:55
     M irc-linux.sh
    ?? docker.sh

--- irc-linux.sh
+++ irc-linux.sh
@@ -4,7 +4,7 @@ irc-linux() {
     local day_back="$1"
     [ -z "$day_back" ] && day_back=0
     #wget -qO- $(LANG=C TZ=UTC date --date="-${day_back} day" +http://irc.linsovet.org.ua/logs/linux_raw/linux.log.%d%b%Y.txt)
-    wget -qO- $(LANG=C TZ=UTC date --date="-${day_back} day" +http://irc.linsovet.org.ua/logs/linux_raw/linux.log.%d%b%Y.txt)|
+    wget -qO- $(LC_ALL=en_US.utf8 LANG=C TZ=UTC date --date="-${day_back} day" +http://irc.linsovet.org.ua/logs/linux_raw/linux.log.%d%b%Y.txt)|
     grep -av '#linux: mode change'|
     grep -avP '\([^\)]+\) joined #linux'|
     grep -avP '\([^\)]+\) left irc:'|
